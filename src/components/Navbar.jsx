dcb8477f15f8f1a4cc8f98af8a42e1957b3c1b9b
Cristian Predusca <cristian.predusca@oveit.com>
2025-07-08 20:09:20 +0300

    am creat navbarul si l-am adaugat in App

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
+
+// main / production / prod
+// staging
+
+// Pasi creare branch
+
+//  Implement TanStack Query to ScanPage -> "t-63/implement-tanstack-query-to-scan-page"
